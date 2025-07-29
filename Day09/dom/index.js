44cf3755735e18ae217c81f72ac196da3193b4bd
minyeong123 <jmy0483@naver.com>
2025-07-29 21:40:43 +0900

    Add files via upload

---
+++ Day09/dom/index.js
@@ -0,0 +1,32 @@
+// document [html 타입]
+// element [tag 타입]
+
+//1. querySelector/querySelectorAll: 찾아주세요
+const a = document.body.querySelector(".hi");
+a.innerHTML = "수고하세요";
+
+const b = document.body.querySelector(".text");
+b.innerHTML = "살려줘";
+
+//time 현재시간 나오도록
+const c = document.body.querySelector(".time");
+c.innerHTML = new Date().toLocaleTimeString();
+
+const month = document.body.querySelector(".month");
+month.innerHTML = new Date().getMonth() +1 +"월";
+
+const date = document.body.querySelector(".date");
+date.innerHTML = new Date().getDate()+ "일";
+
+const day= document.body.querySelector(".day");
+const dayList = ["일","월","화","수","목","금","토"]
+day.innerHTML = dayList[new Date().getDay()] + "요일";
+
+const hour= document.body.querySelector(".hour");
+hour.innerHTML = new Date().getHours() + "시";
+
+const minute= document.body.querySelector(".minute");
+minute.innerHTML = new Date().getMinutes() + "분";
+
+const second= document.body.querySelector(".second");
+second.innerHTML = new Date().getSeconds() + "초";
---
+++ Day10/render/index.js
@@ -0,0 +1,36 @@
+const clothes = [
+    {
+    src : "aa.jpg",
+    brand:"무신사 스탠다드",
+    name:"플렉스 포스",
+    price: "93,420원",
+    },
+    {
+    src : "aa.jpg",
+    brand:"무신사 스탠다드",
+    name:"플렉스 포스",
+    price: "93,420원",
+    },
+        {
+    src : "aa.jpg",
+    brand:"무신사 스탠다드",
+    name:"플렉스 포스",
+    price: "93,420원",
+    },
+];
+
+const itemTag =  (item) => `
+    <div class="item">
+                <img src="${item.src}" alt="">
+                <div class="info">
+                    <span class="brand">${item.brand}</span>
+                    <span class="name">${item.name}</span>
+                    <span class="price">${item.price}</span>
+                </div>
+            </div>
+`;
+
+const box = document.querySelector(".box");
+clothes.forEach((x)=> {
+    box.insertAdjacentHTML("beforeend",itemTag(x))
+})
